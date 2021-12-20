981c97e3218d97429a0791b66e09d123ef885094
Jkod <johan.arenas.123123@gmail.com>
2021-12-20 16:59:15 -0500

    Feature: creando función que nos permite hacer una suma.

--- src/app.py
+++ src/app.py
@@ -1,8 +1,12 @@
 from flask import Flask
 
-
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return "Hello Jkod"
+
+@app.route('/sum/<int:a>/<int:b>')
+def sum(a: int, b: int):
+    nums_sum = a + b
+    return f"La suma es: {str(nums_sum)}"
