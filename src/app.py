c0a29df14afac0ec08763e2fe262d8acbe310b9e
Jkrox <83181939+Jkrox@users.noreply.github.com>
2021-12-25 19:39:34 -0500

    Release: añadiendo función para multiplicar.

--- src/app.py
+++ src/app.py
@@ -14,3 +14,8 @@ def greeting():
 def sum(a: int, b: int):
     nums_sum = a + b
     return f"La suma es: {str(nums_sum)}"
+
+@app.route('/multiply/<int:a>/<int:b>')
+def multiply(a: int, b: int):
+    result = float(a * b)
+    return f"Resultado de la multiplicación: {str(result)}"
